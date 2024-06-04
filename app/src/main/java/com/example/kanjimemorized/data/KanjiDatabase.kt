0ca2161691eb8878c65c8c947e6b99e78bb7c552
Joshua Peters <petersjoshuaedu@gmail.com>
2024-06-04 17:15:43 -0400

    working on migrating kanji data when more kanji is added

--- app/src/main/java/com/example/kanjimemorized/data/KanjiDatabase.kt
+++ app/src/main/java/com/example/kanjimemorized/data/KanjiDatabase.kt
@@ -1,8 +1,10 @@
 package com.example.kanjimemorized.data
 
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.room.migration.AutoMigrationSpec
 import com.example.kanjimemorized.data.entities.Kanji
 import com.example.kanjimemorized.data.entities.Review
 import com.example.kanjimemorized.data.entities.relations.KanjiComponentCrossRef
@@ -11,7 +13,10 @@ import com.example.kanjimemorized.data.entities.relations.KanjiMeaningCrossRef
 @Database(
     entities = [Kanji::class, Review::class, KanjiMeaningCrossRef::class, KanjiComponentCrossRef::class],
     version = 1,
-    exportSchema = true
+    exportSchema = true,
+    autoMigrations = [
+        AutoMigration(from = 1, to = 2)
+    ]
 )
 @TypeConverters(
     value = [Converter::class]
