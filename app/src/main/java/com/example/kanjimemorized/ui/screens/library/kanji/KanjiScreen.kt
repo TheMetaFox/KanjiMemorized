e17a3e22d1213b3af35adccca101eadbfa30374c
Joshua Peters <petersjoshuaedu@gmail.com>
2025-01-14 16:35:06 -0500

    clean-up

--- app/src/main/java/com/example/kanjimemorized/ui/screens/library/kanji/KanjiScreen.kt
+++ app/src/main/java/com/example/kanjimemorized/ui/screens/library/kanji/KanjiScreen.kt
@@ -50,7 +50,6 @@ import androidx.compose.ui.platform.UriHandler
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
